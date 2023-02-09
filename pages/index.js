ec40de1a40d2aa43660a865eb466d61c9da5056b
Mayco Barale <maycobarale@gmail.com>
2023-02-09 18:41:42 -0300

    change image sizes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+// import Head from "next/head";
 import Image from "next/image";
 import fs from "fs/promises";
 import Link from "next/link";
@@ -7,29 +7,22 @@ import Layout from "./components/Layout";
 export default function Home({ latestComics }) {
   return (
     <>
-      <Head>
-        <title>xkcd - Comics for developers</title>
-        <meta name="description" content="xkcd - Comics for developers" />
-      </Head>
       <Layout>
         <h2 className="text-3xl font-bold text-center mb-10">Latest Comics</h2>
-        <section className="grid grid-cols-1 gap-2 max-w-md m-auto sm:grid-cols-2 md:grid-cols-3">
+        <section className="grid grid-cols-1 gap-2 m-auto sm:grid-cols-2 md:grid-cols-3 ">
           {latestComics.map((comic) => (
-            <div key={comic.id}>
-              <Link href={`/comic/${comic.id}`}>
-                <a className="mb-4 pb-4 m-auto">
-                  <h3 className="font-bold text-center pb-2">{comic.title}</h3>
-                  <Image
-                    src={comic.img}
-                    width={comic.width}
-                    // width={200}
-                    height={comic.height}
-                    // height={200}
-                    alt={comic.alt}
-                  />
-                </a>
-              </Link>
-            </div>
+            <Link href={`/comic/${comic.id}`} key={comic.id}>
+              <a className="mb-4 pb-4 m-auto">
+                <h3 className="font-bold text-center pb-2">{comic.title}</h3>
+                <Image
+                  src={comic.img}
+                  width={250}
+                  height={250}
+                  alt={comic.alt}
+                  objectFit="contain"
+                />
+              </a>
+            </Link>
           ))}
         </section>
       </Layout>
