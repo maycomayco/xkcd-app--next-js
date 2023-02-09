bb43447eb506bbdbea9a59c1e806864045ccc734
Mayco Barale <maycobarale@gmail.com>
2023-02-09 18:41:20 -0300

    update footer component

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center p-4 max-w-xl m-auto">
+    <footer className="flex justify-center items-center p-4 max-w-xl m-auto font-semibold">
       <a href="https://xkcd.com" target="_blanck" rel="noopener noreferrer">
         All comic by xkcd.com
       </a>
